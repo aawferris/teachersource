1b33846675b9d566413fa06377c288f79831d4b8
James Mernin <jamespmernin@gmail.com>
2020-10-29 16:50:13 -0400

    Semicolons added

--- controllers/lessons.js
+++ controllers/lessons.js
@@ -1,64 +1,64 @@
-const Lesson = require('../models/lesson')
-const db = require('../db/connection')
+const Lesson = require('../models/lesson');
+const db = require('../db/connection');
 
-db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 const getLessons = async (req, res) => {
   try {
-    const lessons = await Lesson.find()
-    res.json(lessons)
+    const lessons = await Lesson.find();
+    res.json(lessons);
   } catch (error) {
-    res.status(500).json({ error: error.message })
+    res.status(500).json({ error: error.message });
   }
 }
 
 const getLesson = async (req, res) => {
   try {
-    const { id } = req.params
-    const lesson = await Lesson.findById(id)
+    const { id } = req.params;
+    const lesson = await Lesson.findById(id);
     if (lesson) {
-      return res.json(lesson)
+      return res.json(lesson);
     }
-    res.status(404).json({ message: 'Lesson not found!' })
+    res.status(404).json({ message: 'Lesson not found!' });
   } catch (error) {
-    res.status(500).json({ error: error.message })
+    res.status(500).json({ error: error.message });
   }
 }
 
 const createLesson = async (req, res) => {
   try {
-    const lesson = new Lesson(req.body)
-    await lesson.save()
-    res.status(201).json(lesson)
+    const lesson = new Lesson(req.body);
+    await lesson.save();
+    res.status(201).json(lesson);
   } catch (error) {
-    console.log(error)
-    res.status(500).json({ error: error.message })
+    console.log(error);
+    res.status(500).json({ error: error.message });
   }
 }
 
 const updateLesson = async (req, res) => {
-  const { id } = req.params
+  const { id } = req.params;
   await Lesson.findByIdAndUpdate(id, req.body, { new: true }, (error, lesson) => {
     if (error) {
-      return res.status(500).json({ error: error.message })
+      return res.status(500).json({ error: error.message });
     }
     if (!lesson) {
-      return res.status(404).json(lesson)
+      return res.status(404).json(lesson);
     }
-    res.status(200).json(lesson)
+    res.status(200).json(lesson);
   })
 }
 
 const deleteLesson = async (req, res) => {
   try {
-    const { id } = req.params
-    const deleted = await Lesson.findByIdAndDelete(id)
+    const { id } = req.params;
+    const deleted = await Lesson.findByIdAndDelete(id);
     if (deleted) {
-      return res.status(200).send("Lesson deleted")
+      return res.status(200).send("Lesson deleted");
     }
-    throw new Error("Lesson not found")
+    throw new Error("Lesson not found");
   } catch (error) {
-    res.status(500).json({ error: error.message })
+    res.status(500).json({ error: error.message });
   }
 }
 
