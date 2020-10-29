48e1d8e502175a164f9892c176f29e69cc94e4c7
James Mernin <jamespmernin@gmail.com>
2020-10-29 16:42:45 -0400

    Finished controller and database files

---
+++ db/connection.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+
+let MONGODB_URI = process.env.PROD_MONGODB || 'mongodb://127.0.0.1:27017/lessonsAppDatabase'
+
+mongoose
+  .connect(MONGODB_URI, { useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false })
+  .then(() => console.log('Successfully connected to MongoDB.'))
+  .catch(event => console.error('Connection error', event.message))
+
+module.exports = mongoose.connection
