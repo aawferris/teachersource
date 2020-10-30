606cfe5b70469a2b520d3ef8e62d70d8cc9fecdf
Andrew <aawferris@gmail.com>
2020-10-30 16:47:16 -0400

    rendering images

--- client/src/components/LessonCard/LessonCard.jsx
+++ client/src/components/LessonCard/LessonCard.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import "./LessonCard.css";
 import { Link } from "react-router-dom";
+import img from "./file-icon.png"
 
 const LessonCard = (props) => {
   return (
     <div className="lesson-card">
       <Link className="card" to={`/lessons/${props._id}`}>
-        <div>View</div>
+        <div>
+          <img src={img}/>
+        </div>
       </Link>
     </div>
   );
