53833c957d4284ac1c4113e7b3e018fdfd1ff915
Andrew <aawferris@gmail.com>
2020-11-02 11:02:04 -0500

    styled the edit lesson page and adjusted the position of the footer

--- client/src/screens/EditLesson/EditLesson.jsx
+++ client/src/screens/EditLesson/EditLesson.jsx
@@ -46,49 +46,76 @@ const LessonEdit = (props) => {
 
   return (
     <Layout>
+      <div className="edit-header">
+        <img id="arrow" src="/assets/arrow-icon.png" alt="backpoiting arrow"/>
+        <h1 id="edit-lesson-plan">Edit Lesson Plan</h1>
+      </div>
       <div className="lesson-edit">
         <form className="edit-form" onSubmit={handleSubmit}>
-          <input
-            className="input-title"
-            type="text"
-            placeholder='Title'
-            value={lesson.title}
-            name='title'
-            required
-            autoFocus
-            onChange={handleChange}
-          />
-          <input
-            className="input-subject"
-            type="text"
-            placeholder='Subject'
-            value={lesson.subject}
-            name='subject'
-            required
-            autoFocus
-            onChange={handleChange}
-          />
-          <input
-            className="input-grade-level"
-            type="text"
-            placeholder='Grade Level'
-            value={lesson.gradeLevel}
-            name='gradeLevel'
-            required
-            autoFocus
-            onChange={handleChange}
-          />
-          <textarea
-            className="textarea-description"
-            type="textarea"
-            rows={10}
-            cols={78}
-            placeholder='Desciption'
-            value={lesson.description}
-            name='description'
-            required
-            onChange={handleChange}
-          />
+          <div className="title-gl-subj-box">
+            <div className="title-box">
+              <input
+                className="input-title"
+                type="text"
+                // placeholder='Title'
+                value={lesson.title}
+                name='title'
+                required
+                autoFocus
+                onChange={handleChange}
+              />
+              <label for="title">Title</label>
+            </div>
+            <div className="grade-box">
+              <input
+                className="input-grade-level"
+                type="text"
+                // placeholder='Grade Level'
+                value={lesson.gradeLevel}
+                name='gradeLevel'
+                required
+                autoFocus
+                onChange={handleChange}
+              />
+              <label for="gradeLevel">Grade Level</label>
+            </div>
+            <div className="subject-box">
+                <input
+                  className="input-subject"
+                  type="text"
+                  // placeholder='Subject'
+                  value={lesson.subject}
+                  name='subject'
+                  required
+                  autoFocus
+                  onChange={handleChange}
+                  />
+                <label for="subject">Subject</label>
+              </div>
+            </div> 
+          <div className="description-box">
+            <textarea
+              className="textarea-description"
+              type="textarea"
+              rows={10}
+              cols={78}
+              // placeholder='Desciption'
+              value={lesson.description}
+              name='description'
+              required
+              onChange={handleChange}
+            />
+            <label for="description">Description</label>
+          </div>
+          <div id="tag-box">
+            <input
+              className="input-tags"
+              type="tag"
+              value={lesson.subject}
+              name='tag'
+              autoFocus/>
+            <label for="tags">Tags</label>
+          </div>
           <button type='submit' className="save-button">Save</button>
         </form>
       </div>
