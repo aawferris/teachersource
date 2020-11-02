e80a4045e2b463306687075f55a9f912473d9f1d
jairo <jairo.angulo93@gmail.com>
2020-11-02 15:26:48 -0500

    homepage stayled with cards

--- client/src/screens/Home/Home.jsx
+++ client/src/screens/Home/Home.jsx
@@ -16,19 +16,11 @@ const Home = () => {
             </div>
           </div>
         </div>
-        <div className="bottom-box">
-          <div id="card-box">
-            <img src="/assets/training-card.png" />
-            <img src="/assets/lesson-card.png" />
-            <img src="/assets/resources-card.png" />
-            <img src="/assets/forums-card.png" />
-          </div>
-          <div className="card-containers">
-            <h2>Discover Training</h2>
-            <h2>Discover Lesson Plans</h2>
-            <h2>Discover Resources</h2>
-            <h2>Discover Forums</h2>
-          </div>
+        <div className="card-box">
+            <button className="card"><h3 className="card-text">Discover Training</h3></button>
+            <button className="card"><h3 className="card-text">Discover Lesson Plans</h3></button>
+            <button className="card"><h3 className="card-text">Discover Resources</h3></button>
+            <button className="card"><h3 className="card-text">Discover Forums</h3></button>
         </div>
       </div>
     </Layout>
