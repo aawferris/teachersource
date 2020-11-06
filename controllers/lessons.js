a91006a401009c23d2bd76a47bcf5cd2e2ac58bb
Andrew <aawferris@gmail.com>
2020-11-06 10:31:21 -0500

    add backend user schema and routing

--- controllers/lessons.js
+++ controllers/lessons.js
@@ -1,7 +1,8 @@
-const Lesson = require('../models/lesson');
-const db = require('../db/connection');
+const Lesson = require("../models/lesson");
+const User = require("../models/user");
+const db = require("../db/connection");
 
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 const getLessons = async (req, res) => {
   try {
@@ -10,7 +11,7 @@ const getLessons = async (req, res) => {
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
-}
+};
 
 const getLesson = async (req, res) => {
   try {
@@ -19,35 +20,40 @@ const getLesson = async (req, res) => {
     if (lesson) {
       return res.json(lesson);
     }
-    res.status(404).json({ message: 'Lesson not found!' });
+    res.status(404).json({ message: "Lesson not found!" });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
-}
+};
 
 const createLesson = async (req, res) => {
   try {
     const lesson = new Lesson(req.body);
     await lesson.save();
-    res.status(201).json(lesson);
+    res.status(201).json(lesson); // use this to model for the user create - - tkae the user obj we get (in React and put it in State) -- in the app.js (for accessibility)
   } catch (error) {
     console.log(error);
     res.status(500).json({ error: error.message });
   }
-}
+};
 
 const updateLesson = async (req, res) => {
   const { id } = req.params;
-  await Lesson.findByIdAndUpdate(id, req.body, { new: true }, (error, lesson) => {
-    if (error) {
-      return res.status(500).json({ error: error.message });
+  await Lesson.findByIdAndUpdate(
+    id,
+    req.body,
+    { new: true },
+    (error, lesson) => {
+      if (error) {
+        return res.status(500).json({ error: error.message });
+      }
+      if (!lesson) {
+        return res.status(404).json(lesson);
+      }
+      res.status(200).json(lesson);
     }
-    if (!lesson) {
-      return res.status(404).json(lesson);
-    }
-    res.status(200).json(lesson);
-  })
-}
+  );
+};
 
 const deleteLesson = async (req, res) => {
   try {
@@ -60,12 +66,63 @@ const deleteLesson = async (req, res) => {
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
-}
+};
+
+const getUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+// .populate("lessons"); -- FOR ITERATION 3
+
+const createUser = async (req, res) => {
+  try {
+    const user = new User(req.body);
+    await user.save();
+    res.status(201).json(user); // use this to model for the user create - - tkae the user obj we get (in React and put it in State) -- in the app.js (for accessibility)
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: error.message });
+  }
+};
+
+const updateUser = async (req, res) => {
+  const { id } = req.params;
+  await User.findByIdAndUpdate(id, req.body, { new: true }, (error, user) => {
+    if (error) {
+      return res.status(500).json({ error: error.message });
+    }
+    if (!user) {
+      return res.status(404).json(user);
+    }
+    res.status(200).json(user);
+  });
+};
+
+const deleteUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleted = await User.findByIdAndDelete(id);
+    if (deleted) {
+      return res.status(200).send("User deleted");
+    }
+    throw new Error("User not found");
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 
 module.exports = {
   createLesson,
   getLessons,
   getLesson,
   updateLesson,
-  deleteLesson
-}
+  deleteLesson,
+  getUsers,
+  createUser,
+  updateUser,
+  deleteUser,
+};
