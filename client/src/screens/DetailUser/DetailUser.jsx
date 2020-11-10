4b186e92f6d2f7e029abf0c17cbbf94bc2fb2f95
Andrew <aawferris@gmail.com>
2020-11-10 13:43:41 -0500

    fixed detail user page

--- client/src/screens/DetailUser/DetailUser.jsx
+++ client/src/screens/DetailUser/DetailUser.jsx
@@ -27,13 +27,13 @@ function DetailUser() {
     <Layout>
       <div className="user-detail">
         <div id="details-header">
-          <NavLink id="arrow-link" to="/dashboard">
-            <img id="arrow" src="/assets/arrow-icon.png" alt="backpointing arrow" />
+          <NavLink id="arrow-link" to="/login">
+            <img id="arrow" src="/assets/arrow-go-back.png" alt="backpointing arrow" />
           </NavLink>
           <h1 id="details-h1">User Details</h1>
         </div>
-        <div className="info-box">
-          <div className='details-box'>
+        <div id="info-box">
+          <div id='details-box'>
             <div id='fullname'><span>Name: </span>{user.fullname}</div>
             <div id='email'><span>Email: </span>{user.email}</div>
             <div id='state'><span>State: </span>{user.state}</div>
@@ -42,8 +42,8 @@ function DetailUser() {
             <div id="subjects"><span>Subjects: </span>{user.subjects}</div>
           </div>
           <div className="button-container">
-            <button className="edit-button"><Link className="edit-link" to={`/users/${user._id}/edit`}>Edit</Link></button>
-            <button className="delete-button"><Link className="delete-link" to={`/users/${user._id}/delete`}>Delete</Link></button>
+          <Link className="edit-link" to={`/users/${user._id}/edit`}><button className="edit-button">Edit</button></Link>
+          <Link className="delete-link" to={`/users/${user._id}/delete`}><button className="delete-button">Delete</button></Link>
           </div>
         </div>
       </div>
